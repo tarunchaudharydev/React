0614aebf41a428a4c1dfdbdbaf323efbc9a39cb1
tarunchaudharydev <tarunchaudhary.dev@gmail.com>
2026-02-28 09:46:22 +0530

    complete day-1

---
+++ Day-1/React-project1/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <p>
+        Hello world! This is my first React project. I'm excited to learn and build amazing things with React!
+      </p>
+    </div>
+  )
+}
+
+export default App
